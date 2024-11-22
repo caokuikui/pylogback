29755714cc9deedb93eafce68f3397f113cbcc8b
ckk <ckk@email.com>
2024-11-22 16:57:05 +0800

    初始化

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
-from .handlers import LogbackHandler, AsyncLogbackHandler
+from .handlers import AsyncLogbackHandler
 from .policies import TimeBasedRollingPolicy, SizeBasedTriggeringPolicy
 from .filters import ContextFilter
 from .configurator import LogbackConfigurator
 
 __version__ = '1.0.0'
 __all__ = [
-    'LogbackHandler',
     'AsyncLogbackHandler',
     'TimeBasedRollingPolicy',
     'SizeBasedTriggeringPolicy',
